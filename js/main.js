65dc5b3e97118ba153e175a149d67b69e8b1fe77
Murilo <eth0.db0@gmail.com>
2021-12-11 15:40:52 -0300

    control

--- js/main.js
+++ js/main.js
@@ -2,6 +2,37 @@ var BASE64_MARKER = ';base64,';
 var qtdCampos = 0;
 var autor = '';
 
+function test(key, value) {
+    var repeater = $('form.repeater-lista');
+    repeater.slideDown();
+    console.log(repeater);
+}
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {   
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
 function convertDataURIToBinary(dataURI) {
     var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
     var base64 = dataURI.substring(base64Index);
